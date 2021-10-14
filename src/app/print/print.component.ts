44b5e40681c73cddf6dffae55962dbc077b18f36
Noam <noam@fireflymigration.com>
2021-10-14 18:37:19 +0300

    started with print

--- src/app/print/print.component.ts
+++ src/app/print/print.component.ts
@@ -10,13 +10,29 @@ import { Delivery } from '../deliveries/delivery';
 export class PrintComponent implements OnInit {
 
   constructor(private remult: Remult) { }
-  deliveries = this.remult.repo(Delivery).find({
-    limit: 1000,
-    where: d => Delivery.activeDeliveryFilter(),
-    orderBy: d => [d.pickupCity, d.pickupDate!, d.deliveryCity, d.deliveryDate!, d.number]
-  })
-  ngOnInit(): void {
+  deliveries: Delivery[] = [];
+  async ngOnInit() {
+    this.deliveries = await this.remult.repo(Delivery).find({
+      limit: 1000,
+      where: d => Delivery.activeDeliveryFilter(),
+      orderBy: d => [d.pickupCity, d.pickupDate!, d.deliveryCity, d.deliveryDate!, d.number]
+    })
+  }
+  contentText(d: Delivery) {
+    let r = "";
+    if (d.surfaces) {
+      r = d.surfaces.toString();
+    }
+    else for (const f of [d.$.laptops, d.$.screens, d.$.computers, d.$.other]) {
+      if (f.value) {
+        if (r != '')
+          r += ", ";
+        r += f.value + " " + f.metadata.caption;
+      }
+    }
+    if (d.notes)
+      r += " - " + d.notes;
+    return r;
   }
 
 }
- 
